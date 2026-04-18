1de2cd658391e9572bab9ef5deff70f21bdf24be
Rakhi Kumari <rakhiwadhwani47@gmail.com>
2026-04-18 21:56:13 +0500

    setup.py done

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+num = int(input("enter number :"))
+ 
+if num > 10: and num % 10 == 0:
+    print ("divisble :")
+else:
+    print("not divisble:")
+
+
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+     """
+    This function returns the list of requirements
+    """
+     
+     requirement_lst:List[str]=[]
+     try:
+          with open('requirements.txt','r') as file:
+               '''read lines from the file'''
+               lines = file.readlines()
+               '''process each line'''
+               for line in lines:
+                    requirement=line.strip()
+                    '''ignore empty lines and -e .'''
+                    if requirement and requirement!= "-e .":
+                         requirement_lst.append(requirement)
+
+
+     except FileNotFoundError:
+          print("requirements.txt file not found")
+
+     return requirement_lst
+
+print(get_requirements())
+
+
+setup(
+     name="NetworkSecurity",
+     version="0.0.1",
+     author="Rakhi kumari",
+     author_email="rakhiwdhwani47@gmail.com",
+     packages=find_packages(),
+     install_requires=get_requirements()
+)
+
+               
